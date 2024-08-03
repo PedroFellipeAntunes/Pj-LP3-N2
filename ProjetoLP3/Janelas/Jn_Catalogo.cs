0faf6daccf21135049f77d68606501b6b0adb3ac
Pedro Antunes <pedroantunes@acad.ifma.edu.br>
2024-08-02 22:00:24 -0300

    update simples de chamada de aluguel em catalogo

--- ProjetoLP3/Janelas/Jn_Catalogo.cs
+++ ProjetoLP3/Janelas/Jn_Catalogo.cs
@@ -64,13 +64,9 @@ namespace ProjetoLP3.Janelas
                 return;
             }
 
-            Jn_Aluguel jn_Aluguel = new Jn_Aluguel(this.MdiParent, this.usuario, filmesSelecionados);
-            //ct_Visual.janelaGrande(jn_Aluguel);
-
-            // Evitar mostrar janela se ela foi fechada por erro
-            if (!jn_Aluguel.IsDisposed)
+            using (Jn_Aluguel jn_Aluguel = new Jn_Aluguel(this.usuario, filmesSelecionados))
             {
-                jn_Aluguel.Show();
+                jn_Aluguel.ShowDialog();
             }
         }
 
